b8038db6ca6b44888047cf17838d4bba27f6d46e
liubinbin <lotosbin@gmail.com>
2015-12-24 10:53:04 +0800

    神道创建日志接口

--- index.js
+++ index.js
@@ -1,6 +1,44 @@
-var request = require('request');
+var request = require('request').defaults({jar: true});
 var config = require('./config');
+function effortcreate(data,efforts) {
+  var formdata={  date:date};
+  for (var i = 0; i < efforts.length; i++) {
+    var effort = efforts[i];
+    formdata["id["+i+"]"]=i;
+    formdata["work["+i+"]"]=effort.work;
+    formdata["consumed["+i+"]"]=2;
+    formdata["objectType["+i+"]"]="custom";
+  }
+  request({
+    url:"http://115.29.238.236:82/zentao/effort-batchCreate-"+date+".html",
+    method:"POST",
+    form:formdata
+  },function(error,response,body){
+    if(error)console.log(error);
+    if(response.statusCode!="200")console.log(response);
+    console.log(body);
+  });
+}
+function todoadd(){
+  //添加日志
+  request({
+    url:"http://115.29.238.236:82/zentao/todo-create-20151224.html",
+    method:"POST",
+    form:{
+      date:"2015-12-24",
+type:"custom",
+pri:3,
+name:"",
+name:"测试创建日志",
+desc:"测试创建日志",
+status:"wait",
+begin:"1030",
+end:"1100",
+    }
+  },function(){
 
+  })
+}
 request({
   url:config.loginurl,
   method:"POST",
@@ -20,4 +58,13 @@ request({
     return;
   }
   console.log(body);
+  //get all my todo
+  // request({url:"http://115.29.238.236:82/zentao/my-todo-all.html",method:"GET"},function(error,response,body){
+  //   console.log(body);
+  // });
+  // todoadd();
+  var efforts=[];
+  efforts.push({work:"测试"});
+  efforts.push({work:"测试"});
+  effortcreate("2015-12-24",efforts);
 })

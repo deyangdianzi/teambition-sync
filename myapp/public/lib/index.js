fcf48ef04d336c5072983bfa81d7a3560a595ae0
liubinbin <lotosbin@gmail.com>
2016-01-09 11:36:39 +0800

    fix bug

--- myapp/public/lib/index.js
+++ myapp/public/lib/index.js
@@ -1,28 +1,28 @@
-export function buglistcontrollerinit(app){
-app.controller('BugListController',['$http','$scope',function($http,$scope){
-   var bugList = this;
-   bugList.list= [];
-   bugList.load = function(){
-	$http.get('/zendao/bug/list').then(function(res){
-		for(i=0;i<res.data.length;i++){
-			bug=res.data[i];
+export function buglistcontrollerinit(app) {
+app.controller('BugListController', ['$http','$scope',function($http, $scope) {
+   const bugList = this;
+   bugList.list = [];
+   bugList.load = function() {
+	$http.get('/zendao/bug/list').then(res => {
+		for(let i=0;i<res.data.length;i++){
+			var bug = res.data[i];
 			bugList.list.push(bug);
 		}
 	});
    }
-   bugList.login=function(){
-   	$http.post('/login',{username:$scope.username,password:$scope.password}).then(function(response){
+   bugList.login = function() {
+   	$http.post('/login',{username:$scope.username,password:$scope.password}).then(response => {
 		if(response.status==200){
-			$scope.user=$scope.username;
+			$scope.user = $scope.username;
 		}else{
-			$scope.user=false;
+			$scope.user = false;
 		}
 	});
     }
-	bugList.logout=function(){
-		$http.post('/logout').then(function(response){
-			if(response.status=200)$scope.user=false;
+	bugList.logout = function() {
+		$http.post('/logout').then(response => {
+			if(response.status=200)$scope.user = false;
 		});
 	};
 }]);
-}
+};
